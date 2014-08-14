073c0214ea5f94b05eeac6e6371db24738820fc8
Dowlath <hibrise@IKRAMs-Mac-mini.local>
2014-08-14 17:40:34 +0530

    array

---
+++ src/Array.java
@@ -0,0 +1,20 @@
+import java.util.jar.Attributes.Name;
+
+
+public class Array {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int number []= {150,415,536,867,989};
+	int n=number.length;
+System.out.println("enter the numbers  ");
+for(int i=0;i<n;i++)
+{
+	System.out.println(" " + number[i]);
+
+}
+}
+}
+
+
